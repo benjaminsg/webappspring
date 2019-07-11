a93045c6ed53ea2d287d86ef8db8e3f473fd0cde
nishadranade <nranade@umass.edu>
2019-07-11 14:34:53 -0400

    getters and setters for config vars

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -10,7 +10,7 @@ public class Greeting {
     private String password;
     private String merchantId;
 
-    //simple auth variables
+    // simple auth variables
     private String AuthReportGroup;
     private String orderId;
     private Long authAmount;
@@ -19,8 +19,8 @@ public class Greeting {
     private String cardNumber;
     private String expirationDate;
     private String cardType;
-    
-    //simple capture variables
+
+    // simple capture variables
     private String captureReportGroup;
     private String cnpTxnId;
     private Long captureAmount;
@@ -29,4 +29,54 @@ public class Greeting {
     private String pin;
     private String captureId;
 
+    // config settings getters and setters
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getProxy() {
+        return this.proxy;
+    }
+
+    public void setProxy(String proxy) {
+        this.proxy = proxy;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser() {
+        return this.user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getMerchantId() {
+        return this.merchantId;
+    }
+
+    public void setMerchantId(String merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    // simpleAuth variable getters and setters
 }
