6aa96b8c135cbf018dbbbd72cc27b52ffa1500c1
nishadranade <nranade@umass.edu>
2019-07-11 14:26:23 -0400

    giant class made

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -2,23 +2,31 @@ package hello;
 
 public class Greeting {
 
-    private long id;
+    // config variables
     private String content;
+    private String url;
+    private String proxy;
+    private String user;
+    private String password;
+    private String merchantId;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+    //simple auth variables
+    private String AuthReportGroup;
+    private String orderId;
+    private Long authAmount;
+    private String authOrderSource;
+    private String authId;
+    private String cardNumber;
+    private String expirationDate;
+    private String cardType;
+    
+    //simple capture variables
+    private String captureReportGroup;
+    private String cnpTxnId;
+    private Long captureAmount;
+    private String paypalNotes;
+    private String captureOrderSource;
+    private String pin;
+    private String captureId;
 
 }
